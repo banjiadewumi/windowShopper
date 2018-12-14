d36dfba4650278fd57680d92731537f99b14d36f
Banji Adewumi <banji@Banjis-Mac.local>
2018-12-13 20:05:25 -0800

    Initial commit

---
+++ windowShopper/Controllers/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  MainVC.swift
+//  windowShopper
+//
+//  Created by Banji Adewumi on 12/13/18.
+//  Copyright © 2018 Banji Adewumi. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
